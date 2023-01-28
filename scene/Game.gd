2fc5b37c6b208676ad845e01a76eb663b7aa8a96
gabrielcancro <gabrielcancro@3ogs.com.ar>
2023-01-28 00:38:11 -0300

    update

--- scene/Game.gd
+++ scene/Game.gd
@@ -1,7 +1,10 @@
 extends Control
 
 func _ready():
-	add_child( preload("res://Levels/Level1.tscn").instance() )
+	var level = preload("res://Levels/Level1.tscn").instance()
+	add_child(level)
+	$Player.position = level.get_node("StartPoint").position
+	level.get_node("StartPoint").visible = false
 
 func _process(delta):
 	$CanvasUI/lb_vel.text = "velocity " + str( int( $Player.velocity.x / 10 ) * 10 )
